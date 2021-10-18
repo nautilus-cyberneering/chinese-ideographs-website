f248a48f63600157f1bb9791e5b88b1513035f79
Jose Celano <josecelano@gmail.com>
2021-10-18 15:33:48 +0100

    [#1] Cp Base images with verbose option

--- scripts/copy-images.sh
+++ scripts/copy-images.sh
@@ -10,4 +10,4 @@ find ./library/data -type f \( -iname \*42*.tif -o -iname \*42*.tiff \)
 
 # Copy all Base images to public folder
 echo "Copying Base images to public folder ..."
-find ./library/data -type f \( -iname \*42*.tif -o -iname \*42*.tiff \) -exec cp {} ./public/images \;
+find ./library/data -type f \( -iname \*42*.tif -o -iname \*42*.tiff \) -exec cp -fv {} ./public/images \;
