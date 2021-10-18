aba65b6bf0eed373cc79df34b054258b4e2a4f78
Jose Celano <josecelano@gmail.com>
2021-10-18 11:55:30 +0100

    [#1] Copy Base images from media library

--- scripts/copy-images.sh
+++ scripts/copy-images.sh
@@ -2,10 +2,12 @@
 
 # Find and copy Base images from data folder to public folder
 
+# TODO: use a domain app to extract the list of Base images. Remove magic numbers.
+
 # Find Base images
 echo "Find Base images ..."
-find ./data -type f \( -iname \*42*.tif -o -iname \*42*.tiff \)
+find ./library/data -type f \( -iname \*42*.tif -o -iname \*42*.tiff \)
 
 # Copy all Base images to public folder
 echo "Copying Base images to public folder ..."
-find ./data -type f \( -iname \*42*.tif -o -iname \*42*.tiff \) -exec cp {} ./public/images \;
+find ./library/data -type f \( -iname \*42*.tif -o -iname \*42*.tiff \) -exec cp {} ./public/images \;
